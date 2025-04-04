2e2a46662836119b10f36797bbb652c1cd82d870
aigib <aigibuht@gmail.com>
2025-04-04 11:09:24 +0300

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,25 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
 
       <p>
+        <strong>Definition: </strong>
+
         {props.meaning.definition}
         <br />
+
+        <strong>Example: </strong>
+
         <em>{props.meaning.example}</em>
       </p>
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
